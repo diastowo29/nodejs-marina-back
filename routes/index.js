f8f36c73dd0ee90088d2226a3a0a08ade57de4ec
Diastowo <diastowo@gmail.com>
2025-01-20 15:07:24 +0700

    remove queue

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var { PrismaClient } = require('@prisma/client');
-const { workQueue, client } = require('../config/redis.config');
+// const { workQueue, client } = require('../config/redis.config');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -12,29 +12,29 @@ router.get('/', async function(req, res, next) {
 });
 
 router.get('/laz', async function(req, res, next) {
-  await client.hSet('lazClient', {
-    accToken: 'tokenab'
-  });
+  // await client.hSet('lazClient', {
+  //   accToken: 'tokenab'
+  // });
   res.status(200).send({});
 })
 
 router.get('/job', async function(req, res, next) {
   // router.get('/jobs', function(req, res, next) {
-    workQueue.getJobs(['paused', 'waiting', 'failed']).then(function(thisJob) {
-      res.status(200).send(thisJob == null ? {} : thisJob)
-    })
+    // workQueue.getJobs(['paused', 'waiting', 'failed']).then(function(thisJob) {
+    //   res.status(200).send(thisJob == null ? {} : thisJob)
+    // })
   // })
 })
 
 router.get('/job/retry',async function(req, res, next) {
-  await workQueue.getJobs(['failed']).then(function(thisJob) {
-    thisJob.forEach(async (failedJob) => {
-      console.log(failedJob.id);
-      let job = await workQueue.getJob(failedJob.id);
-      job.retry();
-      // console.log(job);
-    });
-  })
+  // await workQueue.getJobs(['failed']).then(function(thisJob) {
+  //   thisJob.forEach(async (failedJob) => {
+  //     console.log(failedJob.id);
+  //     let job = await workQueue.getJob(failedJob.id);
+  //     job.retry();
+  //     // console.log(job);
+  //   });
+  // })
   res.status(200).send({});
 })
 
